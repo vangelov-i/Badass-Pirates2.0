c6c0cffe63c67cae1d072756cb4166c77a6115e9
Iliyan Vangelov <iliyan_vangelov@abv.bg>
2015-12-22 01:23:04 +0200

    stats showing validation added (not showing when somebody's dead)

--- Badass Pirates/Badass Pirates/Managers/FontsManager.cs
+++ Badass Pirates/Badass Pirates/Managers/FontsManager.cs
@@ -41,45 +41,51 @@
         public static void Draw(SpriteBatch spriteBatch)
         {
             #region First Player
-
-            FontsManager.hpFont.Draw(
-                spriteBatch,
-                new Vector2(FirstPlayer.Instance.Ship.Position.X, FirstPlayer.Instance.Ship.Position.Y - 20),
-                FirstPlayer.Instance.Ship.Health.ToString());
-            FontsManager.energyFont.Draw(
-                spriteBatch,
-                new Vector2(FirstPlayer.Instance.Ship.Position.X + 70, FirstPlayer.Instance.Ship.Position.Y - 20),
-                FirstPlayer.Instance.Ship.Energy.ToString());
-            FontsManager.shieldFont.Draw(
-                spriteBatch,
-                new Vector2(FirstPlayer.Instance.Ship.Position.X + 40, FirstPlayer.Instance.Ship.Position.Y - 20),
-                FirstPlayer.Instance.Ship.Shields.ToString());
+            if (!FirstPlayer.Instance.Ship.Sunk)
+            {
+                FontsManager.hpFont.Draw(
+                    spriteBatch,
+                    new Vector2(FirstPlayer.Instance.Ship.Position.X, FirstPlayer.Instance.Ship.Position.Y - 20),
+                    FirstPlayer.Instance.Ship.Health.ToString());
+                FontsManager.energyFont.Draw(
+                    spriteBatch,
+                    new Vector2(FirstPlayer.Instance.Ship.Position.X + 70, FirstPlayer.Instance.Ship.Position.Y - 20),
+                    FirstPlayer.Instance.Ship.Energy.ToString());
+                FontsManager.shieldFont.Draw(
+                    spriteBatch,
+                    new Vector2(FirstPlayer.Instance.Ship.Position.X + 40, FirstPlayer.Instance.Ship.Position.Y - 20),
+                    FirstPlayer.Instance.Ship.Shields.ToString());
+            }
 
             #endregion
 
             #region Second Player
-
-            FontsManager.hpFont.Draw(
-                spriteBatch,
-                new Vector2(SecondPlayer.Instance.Ship.Position.X, SecondPlayer.Instance.Ship.Position.Y - 20),
-                SecondPlayer.Instance.Ship.Health.ToString());
-            FontsManager.energyFont.Draw(
-                spriteBatch,
-                new Vector2(SecondPlayer.Instance.Ship.Position.X + 70, SecondPlayer.Instance.Ship.Position.Y - 20),
-                SecondPlayer.Instance.Ship.Energy.ToString());
-            FontsManager.shieldFont.Draw(
-                spriteBatch,
-                new Vector2(SecondPlayer.Instance.Ship.Position.X + 40, SecondPlayer.Instance.Ship.Position.Y - 20),
-                SecondPlayer.Instance.Ship.Shields.ToString());
+            if (!SecondPlayer.Instance.Ship.Sunk)
+            {
+                FontsManager.hpFont.Draw(
+                    spriteBatch,
+                    new Vector2(SecondPlayer.Instance.Ship.Position.X, SecondPlayer.Instance.Ship.Position.Y - 20),
+                    SecondPlayer.Instance.Ship.Health.ToString());
+                FontsManager.energyFont.Draw(
+                    spriteBatch,
+                    new Vector2(SecondPlayer.Instance.Ship.Position.X + 70, SecondPlayer.Instance.Ship.Position.Y - 20),
+                    SecondPlayer.Instance.Ship.Energy.ToString());
+                FontsManager.shieldFont.Draw(
+                    spriteBatch,
+                    new Vector2(SecondPlayer.Instance.Ship.Position.X + 40, SecondPlayer.Instance.Ship.Position.Y - 20),
+                    SecondPlayer.Instance.Ship.Shields.ToString());
+            }
 
             #endregion
 
             #region Boss.Instance.
-
-            FontsManager.hpFont.Draw(
-                spriteBatch,
-                new Vector2(Boss.Instance.Position.X + Boss.Instance.image.Texture.Width/2f - 25, Boss.Instance.Position.Y + 30),
-                Boss.Instance.Health.ToString());
+            if (!Boss.Instance.Sunk)
+            {
+                FontsManager.hpFont.Draw(
+                    spriteBatch,
+                    new Vector2(Boss.Instance.Position.X + Boss.Instance.image.Texture.Width / 2f - 25, Boss.Instance.Position.Y + 30),
+                    Boss.Instance.Health.ToString());
+            }
 
             #endregion
         }
